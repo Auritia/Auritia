c56a5413d324ea9d195cf22082ddcb63c7809b48
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-13 08:43:39 +0200

    add dropdown menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,27 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 fn main() {
+  let file_menu = Submenu::new(
+    "File",
+    Menu::new()
+      .add_item(CustomMenuItem::new("open".to_string(), "Open"))
+      .add_item(CustomMenuItem::new("save".to_string(), "Save Project"))
+      .add_item(CustomMenuItem::new(
+        "save_as".to_string(),
+        "Save Project As...",
+      ))
+      .add_item(CustomMenuItem::new("render".to_string(), "Render Audio"))
+      .add_item(CustomMenuItem::new(
+        "project_info".to_string(),
+        "Project Info",
+      )),
+  );
+  // configure the menu
+  let menu = Menu::new().add_submenu(file_menu);
+
   tauri::Builder::default()
     .on_page_load(|window, _payload| {
       let label = window.label().to_string();
@@ -11,6 +31,7 @@ fn main() {
         println!("got 'clicked' event on window '{}'", label);
       });
     })
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
