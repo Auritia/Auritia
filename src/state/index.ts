7466e690208164c92b606b3cf0906677c47d54b4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-13 13:14:48 +0200

    i forgor awaits 💀

--- src/state/index.ts
+++ src/state/index.ts
@@ -10,30 +10,30 @@ const AURITIA_FILE_FILTER = {
   ],
 };
 
-await listen("open", async (event) => {
+listen("open", async (event) => {
   console.log("open");
   const files = await open(AURITIA_FILE_FILTER);
   console.log(files);
 });
-await listen("save", async (event) => {
+listen("save", async (event) => {
   console.log("save");
   const savePath = await save(AURITIA_FILE_FILTER);
   console.log(savePath);
 });
-await listen("save_as", async (event) => {
+listen("save_as", async (event) => {
   console.log("save_as");
   const savePath = await save(AURITIA_FILE_FILTER);
   console.log(savePath);
 });
-await listen("render", async (event) => {
+listen("render", async (event) => {
   console.log("render");
 });
-await listen("project_info", async (event) => {
+listen("project_info", async (event) => {
   console.log("project_info");
 });
-await listen("preferences", async (event) => {
+listen("preferences", async (event) => {
   console.log("preferences");
 });
-await listen("docs", async (event) => {
+listen("docs", async (event) => {
   console.log("docs");
 });
