34d1ba07a02f00fb1873e9a35426b264cba37b7a
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-13 13:38:13 +0200

    useless goodbye

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,6 @@
 
 mod interface;
 
-use tauri::Manager;
-
 // the payload type must implement `Serialize`.
 // for global events, it also must implement `Clone`.
 #[derive(Clone, serde::Serialize)]
@@ -18,26 +16,6 @@ fn main() {
   let empty_payload = Payload { message: "".into() };
 
   tauri::Builder::default()
-    .setup(|app| {
-      // listen to the `event-name` (emitted on any window)
-      let id = app.listen_global("event-name", |event| {
-        println!("got event-name with payload {:?}", event.payload());
-      });
-      // unlisten to the event using the `id` returned on the `listen_global` function
-      // an `once_global` API is also exposed on the `App` struct
-      app.unlisten(id);
-
-      // emit the `event-name` event to all webview windows on the frontend
-      app
-        .emit_all(
-          "event-name",
-          Payload {
-            message: "Tauri is awesome!".into(),
-          },
-        )
-        .unwrap();
-      Ok(())
-    })
     .on_page_load(|window, _payload| {
       let label = window.label().to_string();
       window.listen("clicked".to_string(), move |_payload| {
