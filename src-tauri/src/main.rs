337d0424c2932198f300c06157536c5f92f608e8
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-13 13:11:24 +0200

    implement base dialogs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,37 +3,94 @@
   windows_subsystem = "windows"
 )]
 
-mod menu;
+mod interface;
+
+use tauri::{Manager, Window};
+
+// the payload type must implement `Serialize`.
+// for global events, it also must implement `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      // listen to the `event-name` (emitted on any window)
+      let id = app.listen_global("event-name", |event| {
+        println!("got event-name with payload {:?}", event.payload());
+      });
+      // unlisten to the event using the `id` returned on the `listen_global` function
+      // an `once_global` API is also exposed on the `App` struct
+      app.unlisten(id);
+
+      // emit the `event-name` event to all webview windows on the frontend
+      app
+        .emit_all(
+          "event-name",
+          Payload {
+            message: "Tauri is awesome!".into(),
+          },
+        )
+        .unwrap();
+      Ok(())
+    })
     .on_page_load(|window, _payload| {
       let label = window.label().to_string();
       window.listen("clicked".to_string(), move |_payload| {
         println!("got 'clicked' event on window '{}'", label);
       });
     })
-    .menu(menu::create_menus())
+    .menu(interface::create_menus())
     .on_menu_event(|event| match event.menu_item_id() {
       "open" => {
-        println!("open clicked!");
+        event
+          .window()
+          .emit("open", Payload { message: "".into() })
+          .unwrap();
+        println!("open clicked!")
       }
       "save" => {
+        event
+          .window()
+          .emit("save", Payload { message: "".into() })
+          .unwrap();
         println!("save Clicked!")
       }
       "save_as" => {
+        event
+          .window()
+          .emit("save_as", Payload { message: "".into() })
+          .unwrap();
         println!("save_as Clicked!")
       }
       "render" => {
+        event
+          .window()
+          .emit("render", Payload { message: "".into() })
+          .unwrap();
         println!("render Clicked!")
       }
       "project_info" => {
+        event
+          .window()
+          .emit("project_info", Payload { message: "".into() })
+          .unwrap();
         println!("project_info Clicked!")
       }
       "preferences" => {
+        event
+          .window()
+          .emit("preferences", Payload { message: "".into() })
+          .unwrap();
         println!("preferences Clicked!")
       }
       "docs" => {
+        event
+          .window()
+          .emit("docs", Payload { message: "".into() })
+          .unwrap();
         println!("docs Clicked!")
       }
       _ => {}
---
+++ src/state/index.ts
@@ -0,0 +1,39 @@
+import { emit, listen } from "@tauri-apps/api/event";
+import { open, save } from "@tauri-apps/api/dialog";
+
+const AURITIA_FILE_FILTER = {
+  filters: [
+    {
+      name: "Auritia Project",
+      extensions: ["aur"],
+    },
+  ],
+};
+
+await listen("open", async (event) => {
+  console.log("open");
+  const files = await open();
+  console.log(files);
+});
+await listen("save", async (event) => {
+  console.log("save");
+  const savePath = await save(AURITIA_FILE_FILTER);
+  console.log(savePath);
+});
+await listen("save_as", async (event) => {
+  console.log("save_as");
+  const savePath = await save(AURITIA_FILE_FILTER);
+  console.log(savePath);
+});
+await listen("render", async (event) => {
+  console.log("render");
+});
+await listen("project_info", async (event) => {
+  console.log("project_info");
+});
+await listen("preferences", async (event) => {
+  console.log("preferences");
+});
+await listen("docs", async (event) => {
+  console.log("docs");
+});
