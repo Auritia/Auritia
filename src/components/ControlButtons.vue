874657551077a789afaf74bf53f986c28123df18
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-15 14:46:18 +0200

    fixed the X button not being red on hover

--- src/components/ControlButtons.vue
+++ src/components/ControlButtons.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex text-theme-700">
-    <button @click="appWindow.isFullscreen().then((isFullscreen) => appWindow.setFullscreen(!isFullscreen))" class="controlButton">
+    <button @click="appWindow.isFullscreen().then((isFullscreen) => appWindow.setFullscreen(!isFullscreen))" class="controlButton hover:bg-theme-400 active:bg-theme-500">
       <i-fluency-fullscreen />
     </button>
 
-    <button @click="appWindow.minimize()" class="controlButton">
+    <button @click="appWindow.minimize()" class="controlButton hover:bg-theme-400 active:bg-theme-500">
       <i-fluency-minimize />
     </button>
 
-    <button @click="appWindow.toggleMaximize()" class="controlButton">
+    <button @click="appWindow.toggleMaximize()" class="controlButton hover:bg-theme-400 active:bg-theme-500">
       <i-fluency-maximize />
     </button>
 
@@ -23,6 +23,6 @@ import { appWindow } from "@tauri-apps/api/window";
 
 <style scoped lang="postcss">
 .controlButton {
-  @apply hover:bg-theme-400 hover:text-white active:bg-theme-500 h-full flex items-center justify-center py-1.5 px-4 text-sm;
+  @apply hover:text-white h-full flex items-center justify-center py-1.5 px-4 text-sm;
 }
 </style>
