bc70e56f48d24313f34d38bb19bbb6511f61bd1a
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-27 09:06:52 +0200

    fix bundler
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,29 +3,29 @@
     <!-- {{ reactive.project.tempo }} -->
     <!-- {{ reactive.project.timeSignature }} -->
     <div class="flex gap-0.5 text-theme-900">
-      <Button hint="Tap to sync the metronome to your speed" @click="tapMetronome()">
+      <Button hint="Tap to sync the metronome to your speed" @click="state.tapMetronome()">
         <i-fluency-tap />
       </Button>
-      <Slider v-model="reactive.project.tempo" :min="1" :max="9999" :step="1.0" :percent="false" :default="128" />
-      <Button hint="Toggles the metronome" @click="toggleMetronome()" :isActive="reactive.isMetronomeEnabled">
+      <Slider v-model="state.reactive.project.tempo" :min="1" :max="9999" :step="1.0" :percent="false" :default="128" />
+      <Button hint="Toggles the metronome" @click="state.toggleMetronome()" :isActive="state.reactive.isMetronomeEnabled">
         <i-fluency-metronome />
       </Button>
-      <Button hint="Loops the selected region" @click="toggleLoop()" :isActive="reactive.isLoopEnabled">
+      <Button hint="Loops the selected region" @click="state.toggleLoop()" :isActive="state.reactive.isLoopEnabled">
         <i-fluency-loop />
       </Button>
       <Button
         hint="Enables auto loop when previewing short samples"
-        @click="toggleLoopPreview()"
-        :isActive="reactive.isLoopPreviewEnabled"
+        @click="state.toggleLoopPreview()"
+        :isActive="state.reactive.isLoopPreviewEnabled"
       >
         <i-fluency-infinateloop />
       </Button>
     </div>
 
     <div class="text-sm flex gap-1">
-      <i-fluency-play @click="play()" :class="reactive.isPlaying && 'text-green-400'" class="clicky" />
-      <i-fluency-stop @click="stop()" class="clicky" />
-      <i-fluency-pause @click="pause()" class="clicky" />
+      <i-fluency-play @click="state.play()" :class="state.reactive.isPlaying && 'text-green-400'" class="clicky" />
+      <i-fluency-stop @click="state.stop()" class="clicky" />
+      <i-fluency-pause @click="state.pause()" class="clicky" />
     </div>
     <div></div>
   </div>
@@ -36,5 +36,5 @@ import { useState } from "~/state";
 import Button from "./shared/Button.vue";
 import Slider from "./shared/Slider.vue";
 
-const { reactive, toggleMetronome, toggleLoopPreview, tapMetronome, toggleLoop, play, stop, pause } = useState();
+const state = useState();
 </script>
