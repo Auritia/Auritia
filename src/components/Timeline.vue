e0fd80372126632d0e2a2fadf0b2e4e6ef444dc3
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-28 08:01:50 +0200

    canvas blurrines breakthrough
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="w-full h-full bg-blue-500">
-    <canvas ref="timeline" class="w-full h-full" :width="100" :height="100"></canvas>
+    <div class="px-2 py-1">
+      <input type="range" step="0.1" min="0.1" max="4" v-model="upsampleValue" />
+      {{ upsampleValue }}
+    </div>
+    <canvas ref="timeline" class="w-full h-full transform origin-top-left"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
 // defineProps<{}>();
-import { onMounted, ref } from "vue";
+import { onMounted, Ref, ref, watch } from "vue";
 import { DynamicCanvas } from "~/logic/DynamicCanvas";
 import { TimelineRenderer } from "~/logic/TimelineRenderer";
 
+let renderer: TimelineRenderer | undefined;
 const timeline = ref<HTMLCanvasElement | undefined>();
+const upsampleValue = ref(1);
+
+watch(upsampleValue, () => {
+  renderer?.changeUpsampling(upsampleValue.value);
+});
 
 onMounted(() => {
-  const renderer = new TimelineRenderer(timeline.value!);
-  renderer.rerender();
+  renderer = new TimelineRenderer(timeline.value!);
+  renderer.draw();
 });
 </script>
 
--- src/logic/DynamicCanvas.ts
+++ src/logic/DynamicCanvas.ts
@@ -5,11 +5,28 @@ export class DynamicCanvas {
   private ctx: CanvasRenderingContext2D;
   private debug = scopedDebug("DynamicCanvas");
 
+  /**
+   * This propertyu multiplies the render scale by an amount to reduce the bluriness canvases have.
+   *
+   * 1x is the default which is very blurry and looks like shit.
+   * 2x looks a lot sharper and normal.
+   * 3x and above looks too sharp as if theres no anti-aliasing.
+   *
+   * It's also possible to set it to numbers less than 1x to achive lower resolutions
+   * kinda like how video games have render resolution options like 0.8 etc
+   */
+  private upsampling = 2;
+
   constructor(public output: HTMLCanvasElement) {
     this.ctx = output.getContext("2d")!; // this will never be undefined lmfao unless shit pc
     this.ctx.imageSmoothingEnabled = false;
     this.attachResize();
-    this.rerender();
+    this.draw();
+  }
+
+  public changeUpsampling(upsampling: number) {
+    this.upsampling = upsampling;
+    this.resize(); // must resize to cause the canvas to apply upsampling change.
   }
 
   private attachResize() {
@@ -19,23 +36,22 @@ export class DynamicCanvas {
   public resize() {
     const parent = this.output.parentElement!;
     this.debug("Resizing timeline", parent.clientWidth, parent.clientHeight);
-    this.output.width = parent.clientWidth;
-    this.output.height = parent.clientHeight;
+    this.output.width = parent.clientWidth * this.upsampling;
+    this.output.height = parent.clientHeight * this.upsampling;
 
-    // you have to rerender in order to make stuff look correct
-    this.rerender();
+    // you have to draw in order to make stuff look correct
+    this.draw();
   }
 
-  public rerender() {
+  public draw() {
     // implement in subclass
-    window.devicePixelRatio = 2;
     this.ctx.clearRect(0, 0, this.output.width, this.output.height);
     this.ctx.fillStyle = "red";
     this.ctx.fillRect(0, 0, this.output.width, this.output.height);
     this.ctx.fillStyle = "white";
 
-    const message = "❌ You didnt implement this dumbass";
-    this.ctx.font = "30px Comic Sans MS";
+    const message = "⚠️ You didnt implement draw() dumbass";
+    this.ctx.font = `${50 * this.upsampling}px Arial`;
     this.ctx.fillText(message, this.output.width / 2 - this.ctx.measureText(message).width / 2, this.output.height / 2);
   }
 }
